ad246d056c137d6fb5fdda646b4e0e21d0c0508b
Christos Lazaridis <clazaridis7@gmail.com>
2023-03-26 18:04:44 +0300

    first commit

---
+++ website/auth.py
@@ -0,0 +1,92 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+auth = Blueprint('auth',__name__)
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in', category = 'success')
+                login_user(user, remember = True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('something went wrong', category='error')
+        else:
+            flash('user does not exist', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+@auth.route('/sign-up', methods=['GET','POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        binance_key = request.form.get('binanceKey')
+        binance_secret = request.form.get('binanceSecret')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email Already Exists', category='error')
+        elif len(email) < 4 :
+            flash('email must be greater than 3 characters', category='error')
+        elif len(first_name) < 2:
+            flash('first name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('passwords must match', category='error')
+        elif len(password1) < 8:
+            flash('password must be at least 8 characters', category='error')
+        elif len(binance_key) < 12:
+            flash('please enter a valid Binance API Authenticator Key')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'), key=binance_key, secret_key = binance_secret)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('account created!', category='success')
+            return redirect(url_for('views.manager'))
+    return render_template("sign_up.html", user=current_user)
+@auth.route('/edit-account', methods=['GET', 'POST'])
+@login_required
+def edit_account():
+    if request.method == 'POST':
+        new_binance_key = request.form.get('binanceKey')
+        new_binance_secret = request.form.get('binanceSecret')
+        passwordc = request.form.get('passwordc')
+        new_password1 = request.form.get('newPassword1')
+        new_password2 = request.form.get('newPassword2')
+
+        user = User.query.filter_by(email = current_user.email).first()
+        if check_password_hash(user.password, passwordc):
+            if new_password1:
+                if new_password1 != new_password2:
+                    flash("the two passwords don't match", category='error')
+                elif len(new_password1) < 8:
+                    flash('password must be at least 8 characters', category='error')
+                else:
+                    current_user.password = generate_password_hash ( new_password1, method='sha256' )
+                    flash('password changes successfully', category= 'success')
+            if new_binance_key :
+                current_user.key = new_binance_key
+                flash ( 'binance API key changed successfully' , category = 'success' )
+            if new_binance_secret :
+                current_user.secret_key = new_binance_secret
+                flash ( 'binance API secret key changed successfully' , category = 'success' )
+        else:
+            flash('the password you entered is not correct', category = 'error')
+
+    return render_template ( "edit_account.html" , user = current_user )
+
---
+++ website/views.py
@@ -0,0 +1,169 @@
+from flask import Blueprint , render_template , request , flash , redirect , url_for
+from flask_login import login_required , current_user
+from . import db
+import json
+from binance.client import Client
+from binance.exceptions import BinanceAPIException
+import datetime
+import pytz
+from .models import User
+
+views = Blueprint ( 'views' , __name__ )
+
+SYMBOLS = [ 'BTC' , 'ETH' , 'USDT' , 'ADA' , 'XRP' ]
+DURATION_OPTIONS = [ '1 month' , '3 months' , '6 months' , '9 months' , '12 months' , 'Lifetime' ]
+
+
+class BinanceAPI:
+    def __init__(self, key, secret_key):
+        self.client = Client(key, secret_key)
+
+    def get_account_info(self):
+        try:
+            return self.client.get_account()
+        except BinanceAPIException as e:
+            flash(str(e), category='error')
+            return None
+
+    def get_my_trades(self, symbol, start_time):
+        try:
+            return self.client.get_my_trades(symbol=symbol, startTime=start_time)
+        except BinanceAPIException as e:
+            flash(str(e), category='error')
+            return None
+
+def validate_inputs(duration, symbol_pair):
+    if symbol_pair not in [f"{s1}{s2}" for s1 in SYMBOLS for s2 in SYMBOLS if s1 != s2]:
+        flash("Invalid symbol pair selected", category='error')
+        return False
+    if duration not in DURATION_OPTIONS:
+        flash("Invalid duration selected", category='error')
+        return False
+    else:
+        return True
+
+def calculate_stats(trades):
+    total_profit = 0
+    total_wins = 0
+    total_losses = 0
+    total_win_amount = 0
+    total_loss_amount = 0
+    for trade in trades:
+        if trade['isBuyer']:
+            profit = float(trade['quoteQty']) - float(trade['quoteQty']) * float(trade['commission']) / 100
+        else:
+            profit = float(trade['quoteQty']) + float(trade['quoteQty']) * float(trade['commission']) / 100
+        total_profit += profit
+        if profit > 0:
+            total_wins += 1
+            total_win_amount += profit
+        else:
+            total_losses += 1
+            total_loss_amount += profit
+    if total_wins > 0:
+        win_ratio = total_wins / (total_wins + total_losses) * 100
+        avg_win = total_win_amount / total_wins
+    else:
+        win_ratio = 0
+        avg_win = 0
+    if total_losses > 0:
+        avg_loss = total_loss_amount / total_losses
+    else:
+        avg_loss = 0
+    return {'total_profit': total_profit, 'total_wins': total_wins, 'total_losses': total_losses,
+            'win_ratio': win_ratio, 'avg_win': avg_win, 'avg_loss': avg_loss}
+@views.route('/manager', methods=['GET', 'POST'])
+@login_required
+def manager():
+    binance_api = BinanceAPI(current_user.key, current_user.secret_key)
+    account_info = binance_api.get_account_info()
+    if not account_info:
+        return redirect(url_for('views.manager'))
+
+    balances = {}
+    for balance in account_info['balances']:
+        asset = balance['asset']
+        free = float(balance['free'])
+        locked = float(balance['locked'])
+        if free > 0 or locked > 0:
+            balances[asset] = free + locked
+
+    duration = request.form.get('duration') if request.method == 'POST' else '1 month'
+    symbol_pair = request.form.get('symbol_pair') if request.method == 'POST' else None
+
+    if not validate_inputs(duration, symbol_pair):
+        flash("Invalid input parameters", category='error')
+        return redirect(url_for('views.manager'))
+
+    if symbol_pair:
+        symbol1, symbol2 = symbol_pair[:3], symbol_pair[3:]
+        if symbol1 not in balances or symbol2 not in balances:
+            flash("You do not have sufficient balance for the selected symbol pair", category='error')
+            return redirect(url_for('views.manager'))
+
+    if duration == '1 month':
+        end_time = datetime.datetime.now(pytz.utc)
+        start_time = end_time - datetime.timedelta(days=30)
+    elif duration == '3 months':
+        end_time = datetime.datetime.now(pytz.utc)
+        start_time = end_time - datetime.timedelta(days=90)
+    elif duration == '6 months':
+        end_time = datetime.datetime.now(pytz.utc)
+        start_time = end_time - datetime.timedelta(days=180)
+    elif duration == '9 months':
+        end_time = datetime.datetime.now(pytz.utc)
+        start_time = end_time - datetime.timedelta(days=270)
+    elif duration == '12 months':
+        end_time = datetime.datetime.now(pytz.utc)
+        start_time = end_time - datetime.timedelta(days=360)
+    elif duration == 'Lifetime':
+        start_time = None
+        end_time = None
+    else:
+        flash("Invalid duration selected", category='error')
+        return redirect(url_for('views.manager'))
+
+    trades = []
+    if symbol_pair and start_time and end_time:
+        trades = binance_api.get_my_trades(symbol_pair, int(start_time.timestamp() * 1000))
+        trades = [trade for trade in trades if start_time.timestamp() * 1000 <= trade['time'] <= end_time.timestamp() * 1000]
+    elif symbol_pair:
+        trades = binance_api.get_my_trades(symbol_pair)
+
+    stats = calculate_stats(trades)
+
+    return render_template('manager.html', balances=balances, trades=trades, stats=stats, duration=duration,
+                           symbol_pair=symbol_pair, user=current_user, symbols=SYMBOLS,
+                           duration_options=DURATION_OPTIONS)
+
+@views.route('/equity', methods=['GET', 'POST'])
+@login_required
+def equity():
+    # Retrieve the user's Binance API credentials from the database
+    user = User.query.filter_by(email=current_user.email).first()
+    api_key = user.key
+    secret_key = user.secret_key
+
+    # Create a Binance client using the API credentials
+    client = Client(api_key, secret_key)
+
+    # Retrieve the user's Binance wallet balances
+    balances = client.get_account()['balances']
+
+    # Filter out balances with zero balance and no usd value
+    balances = [b for b in balances if float(b['free']) + float(b['locked']) > 0 and b['asset'] != 'USDT']
+    for b in balances:
+        symbol = b['asset'] + 'USDT'
+        ticker = client.get_symbol_ticker(symbol=symbol)
+        if ticker is None:
+            b['usd_value'] = 'N/A'
+        else:
+            b['usd_value'] = '{:.2f}'.format(float(ticker['price']) * float(b['free']) + float(ticker['price']) * float(b['locked']))
+
+    # Sort balances by USD value
+    balances = sorted(balances, key=lambda x: float(x['usd_value']), reverse=True)
+
+    return render_template('equity.html', balances=balances, user=current_user)
+@views.route('/', methods =['GET'])
+def home():
+    return render_template('home.html', user=current_user)
